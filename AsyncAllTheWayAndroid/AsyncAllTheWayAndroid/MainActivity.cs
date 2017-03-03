b1a0cc7b303fca8ba1b408351978403d8058becc
Jonathan Goldberger <github@johnnygold.com>
2017-03-02 16:20:19 -1000

    Added Task Cancellation Code

--- AsyncAllTheWayAndroid/AsyncAllTheWayAndroid/MainActivity.cs
+++ AsyncAllTheWayAndroid/AsyncAllTheWayAndroid/MainActivity.cs
@@ -1,4 +1,25 @@
-﻿using Android.App;
+﻿/*
+MIT License
+
+Copyright 2017 Joon Goldberger
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of 
+the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+
+using Android.App;
 using Android.Widget;
 using Android.OS;
 using Android.Content;
@@ -7,6 +28,9 @@ using System.Threading.Tasks;
 using Org.Apache.Http.Client.Params;
 using System.Net.Http;
 using Org.Apache.Http.Client;
+using System;
+using System.Threading;
+using Java.Interop;
 
 namespace AsyncAllTheWayAndroid
 {
@@ -32,10 +56,12 @@ namespace AsyncAllTheWayAndroid
 	{
 		Activity context;
 		HttpClient client;
+		Random rand;
 
 		public AsyncArrayAdapter(Activity context, int resource) : base(context, resource)
 		{
 			this.context = context;
+			rand = new Random(DateTime.Now.Millisecond);
 		}
 
 		public override long GetItemId(int position)
@@ -50,30 +76,82 @@ namespace AsyncAllTheWayAndroid
 
 		public override View GetView(int position, View convertView, ViewGroup parent)
 		{
+			// We will need a cancellationTokenSource so we can cancel the async call if the view moves back on screen 
+			// while text is already being loaded. Without this, if a view is loading some text, but the view moves off
+			// and back on screen, the new load may take less time than the old load and then the old load will 
+			// overwrite the new text load and the wrong data will be displayed. So we will c ancel any async task on a 
+			// recycled view before loading the new text. 
+			CancellationTokenSource cts; 
+
 			View view = convertView; // re-use an existing view, if one is available
-			if (view == null) // otherwise create a new one
+			// Otherwise create a new one
+			if (view == null) {
 				view = context.LayoutInflater.Inflate(Android.Resource.Layout.SimpleListItem1, null);
+			}
+			else
+			{
+				// If view exists, cancel any pending async text loading for this view
+				// by calling cts.Cancel();
+				var wrapper = view.Tag.JavaCast<Wrapper<CancellationTokenSource>>();
+				cts = wrapper.Data;
+
+				// If cancellation has not already been requested, cancel the async task
+				if (!cts.IsCancellationRequested)
+				{
+					cts.Cancel();
+				}
+			}
 
 			TextView textView = view.FindViewById<TextView>(Android.Resource.Id.Text1);
 			textView.Text = "placeholder";
 
+			// Create new CancellationTokenSource for this view's async call
+			cts = new CancellationTokenSource();
+
+			// Add to the Tag property of the view wrapped in a Java.Lang.Object
+			view.Tag = new Wrapper<CancellationTokenSource> { Data = cts };
+
+			// Get the cancellation token to pass into the async method
+			var ct = cts.Token;
+
 			Task.Run(async () => {
-				textView.Text = await GetTextAsync(position);
-			});
+				try
+				{
+					textView.Text = await GetTextAsync(position, ct);
+				}
+				catch (System.OperationCanceledException ex)
+				{
+					Console.WriteLine($"Text load cancelled: {ex.Message}");
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
+			}, ct);
 
 			return view;
 		}
 
-		async Task<string> GetTextAsync(int position)
+		async Task<string> GetTextAsync(int position, CancellationToken ct)
 		{
-			await Task.Delay(300); // to simulate a task that takes longer
+			// Check to see if task was cancelled, if so throw cancelled exception.
+			// Good to check at several points, including just prior to returning the string. 
+			ct.ThrowIfCancellationRequested();
+			await Task.Delay(rand.Next(100,750)); // to simulate a task that takes variable amount of time
+			ct.ThrowIfCancellationRequested();
 			if (client == null)
 				client = new HttpClient();
 			string response = await client.GetStringAsync("http://example.com");
 			string stringToDisplayInList = response.Substring(41, 14) + " " + position.ToString();
+			ct.ThrowIfCancellationRequested();
 			return stringToDisplayInList;
 		}
 
+	}
+
+	public class Wrapper<T> : Java.Lang.Object
+	{
+		public T Data;
 	}
 }
 
